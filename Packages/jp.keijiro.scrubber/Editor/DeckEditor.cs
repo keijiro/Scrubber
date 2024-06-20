14aa6dbe96a1fbe8ffe919ccc772b55c37671072
Keijiro Takahashi <keijiro@gmail.com>
2024-06-20 21:57:18 +0900

    Editor code retabbing

--- Packages/jp.keijiro.scrubber/Editor/DeckEditor.cs
+++ Packages/jp.keijiro.scrubber/Editor/DeckEditor.cs
@@ -3,96 +3,97 @@ using UnityEditor;
 using UnityEditorInternal;
 using Path = System.IO.Path;
 
-namespace Scrubber
+namespace Scrubber.Editor {
+
+[CustomEditor(typeof(Deck))]
+class DeckEditor : UnityEditor.Editor
 {
-    [CustomEditor(typeof(Deck))]
-    class DeckEditor : Editor
+    ReorderableList _pageList;
+
+    void OnEnable()
     {
-        ReorderableList _pageList;
+        _pageList = new ReorderableList(
+            serializedObject, serializedObject.FindProperty("_pages"),
+            true, true, true, true
+        );
+        _pageList.drawHeaderCallback = DrawHeader;
+        _pageList.drawElementCallback = DrawElement;
+        _pageList.onAddCallback = OnAddElement;
+    }
 
-        void OnEnable()
-        {
-            _pageList = new ReorderableList(
-                serializedObject, serializedObject.FindProperty("_pages"),
-                true, true, true, true
-            );
-            _pageList.drawHeaderCallback = DrawHeader;
-            _pageList.drawElementCallback = DrawElement;
-            _pageList.onAddCallback = OnAddElement;
-        }
+    (Rect, Rect, Rect, Rect, Rect) CalculateColumnRects(Rect r)
+    {
+        var h = EditorGUIUtility.singleLineHeight;
+        return (
+            new Rect(r.x, r.y, 80, h),
+            new Rect(r.x + 84, r.y, 14, h),
+            new Rect(r.x + 102, r.y, 14, h),
+            new Rect(r.x + 120, r.y, r.width - 120 - 104, h),
+            new Rect(r.x + r.width - 100, r.y, 100, h)
+        );
+    }
 
-        (Rect, Rect, Rect, Rect, Rect) CalculateColumnRects(Rect r)
-        {
-            var h = EditorGUIUtility.singleLineHeight;
-            return (
-                new Rect(r.x, r.y, 80, h),
-                new Rect(r.x + 84, r.y, 14, h),
-                new Rect(r.x + 102, r.y, 14, h),
-                new Rect(r.x + 120, r.y, r.width - 120 - 104, h),
-                new Rect(r.x + r.width - 100, r.y, 100, h)
-            );
-        }
+    void DrawHeader(Rect rect)
+    {
+        rect.xMin += 14;
+        rect.y++;
+        rect.height = 16;
 
-        void DrawHeader(Rect rect)
-        {
-            rect.xMin += 14;
-            rect.y++;
-            rect.height = 16;
+        var columns = CalculateColumnRects(rect);
 
-            var columns = CalculateColumnRects(rect);
+        GUI.Label(columns.Item1, "Video Name", EditorStyles.label);
+        GUI.Label(columns.Item2, "A", EditorStyles.label);
+        GUI.Label(columns.Item3, "L", EditorStyles.label);
+        GUI.Label(columns.Item4, "Text", EditorStyles.label);
+        GUI.Label(columns.Item5, "Image", EditorStyles.label);
+    }
 
-            GUI.Label(columns.Item1, "Video Name", EditorStyles.label);
-            GUI.Label(columns.Item2, "A", EditorStyles.label);
-            GUI.Label(columns.Item3, "L", EditorStyles.label);
-            GUI.Label(columns.Item4, "Text", EditorStyles.label);
-            GUI.Label(columns.Item5, "Image", EditorStyles.label);
-        }
+    void DrawElement(Rect rect, int index, bool active, bool focused)
+    {
+        rect.y += 2;
 
-        void DrawElement(Rect rect, int index, bool active, bool focused)
-        {
-            rect.y += 2;
+        var columns = CalculateColumnRects(rect);
 
-            var columns = CalculateColumnRects(rect);
+        var e = _pageList.serializedProperty.GetArrayElementAtIndex(index);
 
-            var e = _pageList.serializedProperty.GetArrayElementAtIndex(index);
+        EditorGUI.PropertyField(columns.Item1, e.FindPropertyRelative("videoName"), GUIContent.none);
+        EditorGUI.PropertyField(columns.Item2, e.FindPropertyRelative("autoPlay"), GUIContent.none);
+        EditorGUI.PropertyField(columns.Item3, e.FindPropertyRelative("loop"), GUIContent.none);
+        EditorGUI.PropertyField(columns.Item4, e.FindPropertyRelative("text"), GUIContent.none);
+        EditorGUI.PropertyField(columns.Item5, e.FindPropertyRelative("image"), GUIContent.none);
+    }
 
-            EditorGUI.PropertyField(columns.Item1, e.FindPropertyRelative("videoName"), GUIContent.none);
-            EditorGUI.PropertyField(columns.Item2, e.FindPropertyRelative("autoPlay"), GUIContent.none);
-            EditorGUI.PropertyField(columns.Item3, e.FindPropertyRelative("loop"), GUIContent.none);
-            EditorGUI.PropertyField(columns.Item4, e.FindPropertyRelative("text"), GUIContent.none);
-            EditorGUI.PropertyField(columns.Item5, e.FindPropertyRelative("image"), GUIContent.none);
-        }
+    void OnAddElement(ReorderableList list)
+    {
+        // Append a page element.
+        list.serializedProperty.arraySize += 1;
+        list.index = list.serializedProperty.arraySize - 1;
 
-        void OnAddElement(ReorderableList list)
-        {
-            // Append a page element.
-            list.serializedProperty.arraySize += 1;
-            list.index = list.serializedProperty.arraySize - 1;
+        // Clear the page fields.
+        var e = list.serializedProperty.GetArrayElementAtIndex(list.index);
+        e.FindPropertyRelative("videoName").stringValue = null;
+        e.FindPropertyRelative("autoPlay").boolValue = false;
+        e.FindPropertyRelative("loop").boolValue = false;
+        e.FindPropertyRelative("text").stringValue = null;
+        e.FindPropertyRelative("image").objectReferenceValue = null;
+    }
 
-            // Clear the page fields.
-            var e = list.serializedProperty.GetArrayElementAtIndex(list.index);
-            e.FindPropertyRelative("videoName").stringValue = null;
-            e.FindPropertyRelative("autoPlay").boolValue = false;
-            e.FindPropertyRelative("loop").boolValue = false;
-            e.FindPropertyRelative("text").stringValue = null;
-            e.FindPropertyRelative("image").objectReferenceValue = null;
-        }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
 
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
+        EditorGUILayout.Space();
+        _pageList.DoLayoutList();
 
-            EditorGUILayout.Space();
-            _pageList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
 
-            serializedObject.ApplyModifiedProperties();
-        }
-
-        [MenuItem("Assets/Create/Scrubber/Deck")]
-        public static void CreateDeckAsset()
-        {
-            var asset = ScriptableObject.CreateInstance<Deck>();
-            ProjectWindowUtil.CreateAsset(asset, "New Deck.asset");
-        }
+    [MenuItem("Assets/Create/Scrubber/Deck")]
+    public static void CreateDeckAsset()
+    {
+        var asset = ScriptableObject.CreateInstance<Deck>();
+        ProjectWindowUtil.CreateAsset(asset, "New Deck.asset");
     }
 }
+
+} // namespace Scrubber.Editor
--- Packages/jp.keijiro.scrubber/Editor/PresenterEditor.cs
+++ Packages/jp.keijiro.scrubber/Editor/PresenterEditor.cs
@@ -2,74 +2,80 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
 
-namespace Scrubber
+namespace Scrubber.Editor {
+
+[CustomEditor(typeof(Presenter))]
+class PresenterEditor : UnityEditor.Editor
 {
-    [CustomEditor(typeof(Presenter))]
-    class PresenterEditor : Editor
+    SerializedProperty _jogSpeed;
+    SerializedProperty _jogSense;
+    ReorderableList _deckList;
+
+    void OnEnable()
     {
-        ReorderableList _deckList;
+        _jogSpeed = serializedObject.FindProperty("<JogSpeed>k__BackingField");
+        _jogSense = serializedObject.FindProperty("<JogSense>k__BackingField");
 
-        void OnEnable()
-        {
-            _deckList = new ReorderableList(
-                serializedObject, serializedObject.FindProperty("_decks"),
-                true, true, true, true
-            );
-            _deckList.drawHeaderCallback = DrawHeader;
-            _deckList.drawElementCallback = DrawElement;
-            _deckList.onRemoveCallback = OnRemoveElement;
-        }
+        _deckList = new ReorderableList
+          (serializedObject,
+           serializedObject.FindProperty("<Decks>k__BackingField"),
+           true, true, true, true);
+        _deckList.drawHeaderCallback = DrawHeader;
+        _deckList.drawElementCallback = DrawElement;
+        _deckList.onRemoveCallback = OnRemoveElement;
+    }
 
-        void DrawHeader(Rect rect)
-        {
-            rect.xMin += 14;
-            rect.y++;
-            rect.height = 16;
+    void DrawHeader(Rect rect)
+    {
+        rect.xMin += 14;
+        rect.y++;
+        rect.height = 16;
 
-            GUI.Label(rect, "Deck List", EditorStyles.label);
-        }
+        GUI.Label(rect, "Deck List", EditorStyles.label);
+    }
 
-        void DrawElement(Rect rect, int index, bool active, bool focused)
-        {
-            rect.y += 2;
-            rect.height = EditorGUIUtility.singleLineHeight;
+    void DrawElement(Rect rect, int index, bool active, bool focused)
+    {
+        rect.y += 2;
+        rect.height = EditorGUIUtility.singleLineHeight;
 
-            var e = _deckList.serializedProperty.GetArrayElementAtIndex(index);
-            EditorGUI.PropertyField(rect, e, GUIContent.none);
-        }
+        var e = _deckList.serializedProperty.GetArrayElementAtIndex(index);
+        EditorGUI.PropertyField(rect, e, GUIContent.none);
+    }
 
-        void OnRemoveElement(ReorderableList list)
-        {
-            var prop = list.serializedProperty;
+    void OnRemoveElement(ReorderableList list)
+    {
+        var prop = list.serializedProperty;
 
-            prop.GetArrayElementAtIndex(list.index).objectReferenceValue = null;
-            prop.DeleteArrayElementAtIndex(list.index);
+        prop.GetArrayElementAtIndex(list.index).objectReferenceValue = null;
+        prop.DeleteArrayElementAtIndex(list.index);
 
-            if (list.index >= prop.arraySize - 1)
-                list.index = prop.arraySize - 1;
-        }
+        if (list.index >= prop.arraySize - 1)
+            list.index = prop.arraySize - 1;
+    }
 
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
 
-            EditorGUILayout.Space();
-            _deckList.DoLayoutList();
+        EditorGUILayout.Space();
+        _deckList.DoLayoutList();
 
-            serializedObject.ApplyModifiedProperties();
-        }
+        serializedObject.ApplyModifiedProperties();
+    }
 
-        [MenuItem("GameObject/Scrubber/Presenter", false, 10)]
-        static void CreatePresenter()
-        {
-            var path1 = "Packages/jp.keijiro.scrubber/Runtime/Presenter.prefab";
-            var path2 = "Assets/Scrubber/Runtime/Presenter.prefab";
+    [MenuItem("GameObject/Scrubber/Presenter", false, 10)]
+    static void CreatePresenter()
+    {
+        var path1 = "Packages/jp.keijiro.scrubber/Runtime/Presenter.prefab";
+        var path2 = "Assets/Scrubber/Runtime/Presenter.prefab";
 
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path1) ??
-                         AssetDatabase.LoadAssetAtPath<GameObject>(path2);
+        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path1) ??
+                     AssetDatabase.LoadAssetAtPath<GameObject>(path2);
 
-            var go = PrefabUtility.InstantiatePrefab(prefab);
-            Selection.activeGameObject = (GameObject)go;
-        }
+        var go = PrefabUtility.InstantiatePrefab(prefab);
+        Selection.activeGameObject = (GameObject)go;
     }
 }
+
+} // namespace Scrubber.Editor
